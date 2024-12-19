eb3e131bf722c5037970aa02a04f67f406ec9b0b
alamin <alaminsf40@gmail.com>
2024-12-19 15:48:27 +0600

    loop

---
+++ whileLoop.py
@@ -0,0 +1,18 @@
+#while loop using break
+# num = 1
+# while num <= 50:
+#     print(num)
+#     if num%15 ==0:
+#         continue
+#     num+=1
+
+#while loop using continue
+num = 1
+while num <= 50:
+    num+=1
+    if num%2 ==0:
+        continue
+    print(num)
+    
+
+
