47e858208d422f0083da726d3e32eba5e19b91c5
alamin <alaminsf40@gmail.com>
2024-12-19 15:36:57 +0600

    shot hand if else with exmaple

--- ifelse.py
+++ ifelse.py
@@ -5,5 +5,8 @@ elif num1%2!=0:
     print(f"{num1} is not an even number")
 
 
-#Short hand if else code
+#Short hand if code
 if num1>5: print("The number is greater than 5")
+
+#Short hand if else code
+print("The number is greater than 5") if num1>5 else print("The number is less than or egual of 5")
