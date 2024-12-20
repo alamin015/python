2687f90a4df5e0990b6256dca58004eebed8e8ec
alamin <alaminsf40@gmail.com>
2024-12-20 12:17:40 +0600

    problem solved prime number addition by using for loop

--- problemForLoop.py
+++ problemForLoop.py
@@ -1,3 +1,19 @@
 #First problem. print number from -100 to -10
 for x in range(-100,-9,5):
     print(x)
+
+
+#problem 2: sum all the prime number lies between 1 to 1000
+total =0
+for x in range(2,1001):
+    var=True
+    for y in range(2,int(x**0.5)+1):
+        if x%y==0:
+            var=False
+            break
+    if var:
+        total+=x
+
+print(total)
+
+
